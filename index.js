26a2377b8d9bad3362d3104970a68364230c53c6
Codehackerone <soumyajitdatta123@gmail.com>
2021-07-20 17:07:40 +0530

    menus added

--- index.js
+++ index.js
@@ -13,16 +13,33 @@ app.on('ready',function(){
         protocol:'file:',
         slashes:true
     }));
+    const mainMenu=Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
     mainWindow.on('closed',function(){
         mainWindow=null;
     });
-
-    const mainMenu=Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(mainMenu);
 });
 
 const mainMenuTemplate=[
     {
         label:'File',
+        submenu:[
+            {
+                label:'Add Item',
+                click(){
+                    
+                }
+            },
+            {
+                label:'Remove Item',
+            },
+            {
+                label:'Quit',
+                accelerator:process.platform==='darwin'?'Command+Q':'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
     }
-]
+];
