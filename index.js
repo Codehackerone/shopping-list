03c648acb20d7e8dccda54bd040cc12b4621f6ff
Codehackerone <soumyajitdatta123@gmail.com>
2021-07-20 15:25:22 +0530

    Browser window added

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const {app, BrowserWindow, Menu, ipcMain, dialog}=electron;
 
 let mainWindow;
 
+app.on('ready',function(){
+    mainWindow=new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname:path.join(__dirname,'mainWindow.html'),
+        protocol:'file:',
+        slashes:true
+    }));
+    mainWindow.on('closed',function(){
+        mainWindow=null;
+    });
+});
